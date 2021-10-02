032f39ae02f31b42e92fff92752a2c00d50b339c
Eric <eric.lee1@uwaterloo.ca>
2021-10-02 15:47:40 -0400

    add todolist files to website 2

---
+++ todolist/src/Component/MyApp.js
@@ -0,0 +1,13 @@
+import React from "react"
+import MyHeader from './MyHeader'
+import MyMain from './MyMain'
+
+function MyApp(){
+    return(
+        <div>
+            <MyHeader/>
+            <MyMain/>
+        </div>
+    )
+}
+export default MyApp;
