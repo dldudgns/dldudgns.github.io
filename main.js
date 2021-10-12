45f5bc83730fe8ece4a76c9e9042e98777d80739
Eric <eric.lee2@onsemi.com>
2021-10-12 17:09:57 -0400

    added side panel, started adding pics

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+window.onscroll = function(event) {scrollFunction()};
 
+function scrollFunction() {
+    if (scrollY >8 || window.pageYOffset > 8){
+        document.getElementById("topNavBar").style= "padding:30px";
+        document.getElementById("logo").style.fontSize = "25px";
+        topNavBar.addClass('shrink');
+    } else {
+        document.getElementById("topNavBar").style.padding = "80px";
+        document.getElementById("logo").style.fontSize = "35px";
+    }
+}
