a1308f6763fb070fe8b5ce5bead6840fa2af2b7f
Eric <eric.lee2@onsemi.com>
2021-10-22 17:22:51 -0400

    nothing much

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 window.onscroll = function(event) {scrollFunction()};
 
 function scrollFunction() {
-    if (scrollY >8 || window.pageYOffset > 8){
-        document.getElementById("topNavBar").style= "padding:30px";
+    if (window.scrollTop >8){
+        document.getElementById("topNavBar").style.padding= '30px';
         document.getElementById("logo").style.fontSize = "25px";
         topNavBar.addClass('shrink');
     } else {
