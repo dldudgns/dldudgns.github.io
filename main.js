a3fd2799a1599677a1b9b7dc4c98a5a7c6125d50
Eric <eric.lee1@uwaterloo.ca>
2021-10-24 18:49:07 -0400

    still trying to get scroll stuff

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-window.onscroll = function(event) {scrollFunction()};
+document.body.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (window.scrollTop >8){
-        document.getElementById("topNavBar").style.padding= '30px';
-        document.getElementById("logo").style.fontSize = "25px";
-        topNavBar.addClass('shrink');
+    if (document.body.scrollTop >800||document.documentElement.scrollTop>80){
+        document.getElementsByClassName("right-panel").style.padding= '300px 20px';
+        document.getElementById("topNavBar").style.fontSize = "55px";
+        
     } else {
-        document.getElementById("topNavBar").style.padding = "80px";
-        document.getElementById("logo").style.fontSize = "35px";
+        document.getElementById("contents").style.padding = "80px";
+        document.getElementById("titles").style.fontSize = "35px";
     }
 }
+window.addEventListener("scroll",scrollFunction);
