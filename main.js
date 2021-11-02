50a5d594bdac80569350652f2b2b8018f158abdd
Eric <eric.lee1@uwaterloo.ca>
2021-11-01 23:08:02 -0400

    started the new change

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function scrollevent(){
     }
     if (document.documentElement.scrollTop>100 || document.body.scrollTop>100){
         document.getElementById("topNavBar").style.padding = "0 0 0 0";
-        document.getElementById("logoName").style.fontSize="300%";
-        document.getElementById("logo").style.fontSize="150%";
+        document.getElementById("NavName").style.fontSize="300%";
+        document.getElementById("NavOptions").style.fontSize="150%";
         
         }else {
         document.getElementById("topNavBar").style.padding = "250px 100px 250px 100px";
-        document.getElementById("logoName").style.fontSize="750%";
-        document.getElementById("logo").style.fontSize="200%";
+        document.getElementById("NavName").style.fontSize="750%";
+        document.getElementById("NavOptions").style.fontSize="200%";
         
         // confirm("");
     }
