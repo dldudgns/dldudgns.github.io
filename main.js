747f11d3051e40ac3aecb517db5251ba9d301c26
Eric <eric.lee2@onsemi.com>
2021-11-11 14:33:22 -0500

    "starting on about me"

--- main.js
+++ main.js
@@ -12,52 +12,52 @@ function scrollevent(){
             document.getElementById("NavName").style.fontSize="18vmin";
             document.getElementById("NavOptions").style.fontSize="3.2vmin";
         }
-    if ((getScrollVal())<50){// Education in
-        elementIn(0,10,"title");
-        elementIn(0,20,"logo");
-        elementIn(0,35, "description");
+    if ((getScrollVal())<1050){// Education in
+        elementIn(0,1010,"title");
+        elementIn(0,1020,"logo");
+        elementIn(0,1035, "description");
         // confirm(getBodyScroll());
-    }else if ((getScrollVal())<161){// Education out
-        elementOut(0,95,"logo");
-        elementOut(0,100,"title");
-        elementOut(0,110,"description");
+    }else if ((getScrollVal())<1161){// Education out
+        elementOut(0,1095,"logo");
+        elementOut(0,1100,"title");
+        elementOut(0,1110,"description");
     // Internships & ONSEMI In
-        elementIn(1,115,"title");
-        elementIn(1,140,"logo");
-        elementIn(0,145,"subtitle");
-        elementIn(1,160,"description");
-    }else if ((getScrollVal())<235){
+        elementIn(1,1115,"title");
+        elementIn(1,1140,"logo");
+        elementIn(0,1145,"subtitle");
+        elementIn(1,1160,"description");
+    }else if ((getScrollVal())<1235){
         //Upchain In
-        elementIn(2,180,"logo");
-        elementIn(1,185,"subtitle");
-        elementIn(2,195,"description");
+        elementIn(2,1180,"logo");
+        elementIn(1,1185,"subtitle");
+        elementIn(2,1195,"description");
         //Internship & ONSEMI out
-        elementOut(1,195,"title");
-        elementOut(1,210,"logo");
-        elementOut(0,215,"subtitle");
-        elementOut(1,225,"description");
+        elementOut(1,1195,"title");
+        elementOut(1,1210,"logo");
+        elementOut(0,1215,"subtitle");
+        elementOut(1,1225,"description");
         //UWaterloo In
-        elementIn(3,215,"logo");
-        elementIn(2,220,"subtitle");
-        elementIn(3,230,"description");
-    }else if ((getScrollVal())<315){
+        elementIn(3,1215,"logo");
+        elementIn(2,1220,"subtitle");
+        elementIn(3,1230,"description");
+    }else if ((getScrollVal())<1315){
         //Upchain Out
-        elementOut(2,260,"logo");
-        elementOut(1,265,"subtitle");
-        elementOut(2,275,"description");
+        elementOut(2,1260,"logo");
+        elementOut(1,1265,"subtitle");
+        elementOut(2,1275,"description");
         //Ciena In
-        elementIn(4,250,"logo");
-        elementIn(3,255,"subtitle");
-        elementIn(4,265,"description");
+        elementIn(4,1250,"logo");
+        elementIn(3,1255,"subtitle");
+        elementIn(4,1265,"description");
         //UWaterloo Out
-        elementOut(3,290,"logo");
-        elementOut(2,300,"subtitle");
-        elementOut(3,310,"description");
-    }else if ((getScrollVal())<350){
+        elementOut(3,1290,"logo");
+        elementOut(2,1300,"subtitle");
+        elementOut(3,1310,"description");
+    }else if ((getScrollVal())<1350){
         //Ciena Out
-        elementOut(4,330,"logo");
-        elementOut(3,335,"subtitle");
-        elementOut(4,345,"description");
+        elementOut(4,1330,"logo");
+        elementOut(3,1335,"subtitle");
+        elementOut(4,1345,"description");
     }
         
         
